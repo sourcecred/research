5a8013af8d50850576fd808bd72ffeb6acb5ca6c
Michael Zargham <michael.zargham@gmail.com>
2019-04-20 16:17:46 -0700

    applied black to graph generators

--- infra/graph_generators.py
+++ infra/graph_generators.py
@@ -7,73 +7,77 @@ Created on Sun Mar 17 13:00:33 2019
 """
 import networkx as nx
 
-def lineGraphGen(N, bidir=False,nodeTypeName='vanilla',edgeTypeName='vanilla'):
+
+def lineGraphGen(N, bidir=False, nodeTypeName="vanilla", edgeTypeName="vanilla"):
 
     line = nx.path_graph(N, create_using=nx.MultiDiGraph)
-    if not(bidir):
+    if not (bidir):
         G = line
-    else:  
+    else:
         edges = line.edges
         G = nx.MultiDiGraph()
         for e in edges:
-            G.add_edge(e[0],e[1])
-            G.add_edge(e[1],e[0])
-    
-    nx.set_node_attributes(G,nodeTypeName, 'type')
-    nx.set_edge_attributes(G,edgeTypeName, 'type')
-    
+            G.add_edge(e[0], e[1])
+            G.add_edge(e[1], e[0])
+
+    nx.set_node_attributes(G, nodeTypeName, "type")
+    nx.set_edge_attributes(G, edgeTypeName, "type")
+
     return G
 
-def starGraphGen(N, kind='sink',nodeTypeName='vanilla',edgeTypeName='vanilla'):
-    
+
+def starGraphGen(N, kind="sink", nodeTypeName="vanilla", edgeTypeName="vanilla"):
+
     star = nx.star_graph(N)
     G = nx.MultiDiGraph()
 
     for e in star.edges:
-        if (kind == 'source') or (kind == 'bidir'):
-            G.add_edge(e[0],e[1])
-        if (kind == 'sink') or (kind == 'bidir'):
-            G.add_edge(e[1],e[0])
+        if (kind == "source") or (kind == "bidir"):
+            G.add_edge(e[0], e[1])
+        if (kind == "sink") or (kind == "bidir"):
+            G.add_edge(e[1], e[0])
 
-    nx.set_node_attributes(G,nodeTypeName, 'type')
-    nx.set_edge_attributes(G,edgeTypeName, 'type')
-    
+    nx.set_node_attributes(G, nodeTypeName, "type")
+    nx.set_edge_attributes(G, edgeTypeName, "type")
+
     return G
 
-def circleGraphGen(N, bidir=False,nodeTypeName='vanilla',edgeTypeName='vanilla' ):
-    
+
+def circleGraphGen(N, bidir=False, nodeTypeName="vanilla", edgeTypeName="vanilla"):
+
     circle = nx.cycle_graph(N, create_using=nx.MultiDiGraph)
-    if not(bidir):
+    if not (bidir):
         G = circle
-    else:  
+    else:
         edges = circle.edges
         G = nx.MultiDiGraph()
         for e in edges:
-            G.add_edge(e[0],e[1])
-            G.add_edge(e[1],e[0])
-            
-    nx.set_node_attributes(G,nodeTypeName, 'type')
-    nx.set_edge_attributes(G,edgeTypeName, 'type')
-    
+            G.add_edge(e[0], e[1])
+            G.add_edge(e[1], e[0])
+
+    nx.set_node_attributes(G, nodeTypeName, "type")
+    nx.set_edge_attributes(G, edgeTypeName, "type")
+
     return G
 
-def treeGraphGen(r,h, kind='sink',nodeTypeName='vanilla',edgeTypeName='vanilla'):
-    
-    tree = nx.balanced_tree(r,h, create_using=nx.MultiDiGraph)
-    
-    if kind=='source':
-        G = tree
-    elif kind =='sink':
-        G = nx.MultiDiGraph()
-        for e in tree.edges:
-            G.add_edge(e[1],e[0])
-    elif kind == 'bidir':
-        G = nx.MultiDiGraph()
-        for e in tree.edges:
-            G.add_edge(e[1],e[0])
-            G.add_edge(e[0],e[1])
 
-    nx.set_node_attributes(G,nodeTypeName, 'type')
-    nx.set_edge_attributes(G,edgeTypeName, 'type')
-    
-    return G    
+def treeGraphGen(r, h, kind="sink", nodeTypeName="vanilla", edgeTypeName="vanilla"):
+
+    tree = nx.balanced_tree(r, h, create_using=nx.MultiDiGraph)
+
+    if kind == "source":
+        G = tree
+    elif kind == "sink":
+        G = nx.MultiDiGraph()
+        for e in tree.edges:
+            G.add_edge(e[1], e[0])
+    elif kind == "bidir":
+        G = nx.MultiDiGraph()
+        for e in tree.edges:
+            G.add_edge(e[1], e[0])
+            G.add_edge(e[0], e[1])
+
+    nx.set_node_attributes(G, nodeTypeName, "type")
+    nx.set_edge_attributes(G, edgeTypeName, "type")
+
+    return G
--- infra/graph_generators_test.py
+++ infra/graph_generators_test.py
@@ -9,6 +9,7 @@ import networkx as nx
 import unittest
 from graph_generators import lineGraphGen, starGraphGen, treeGraphGen
 
+
 class GraphGenerators_LineGraph(unittest.TestCase):
     def test_lineGraphGen_sanity(self):
         g = lineGraphGen(4)
@@ -17,52 +18,78 @@ class GraphGenerators_LineGraph(unittest.TestCase):
     def test_lineGraphGen_bidir(self):
         g = lineGraphGen(4, bidir=True)
         # (node, neighbor)
-        self.assertEqual(list(g.edges()), [(0, 1), (1, 0), (1, 2), (2, 1), (2, 3), (3, 2)])
+        self.assertEqual(
+            list(g.edges()), [(0, 1), (1, 0), (1, 2), (2, 1), (2, 3), (3, 2)]
+        )
+
 
 class GraphGenerators_StarGraph(unittest.TestCase):
 
-# how to validate that 'kind' is either 'source', 'sink', or 'bidir' ?? 
+    # how to validate that 'kind' is either 'source', 'sink', or 'bidir' ??
 
     def test_starGraph_sink(self):
         g = starGraphGen(4)
         self.assertEqual(list(g.edges()), [(1, 0), (2, 0), (3, 0), (4, 0)])
 
     def test_starGraph_source(self):
-        g = starGraphGen(4, kind='source')
+        g = starGraphGen(4, kind="source")
         self.assertEqual(list(g.edges()), [(0, 1), (0, 2), (0, 3), (0, 4)])
 
     def test_starGraph_bidir(self):
-        g = starGraphGen(4, kind='bidir')
-        self.assertEqual(list(g.edges()), [(0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (2, 0), (3, 0), (4, 0)])
+        g = starGraphGen(4, kind="bidir")
+        self.assertEqual(
+            list(g.edges()),
+            [(0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (2, 0), (3, 0), (4, 0)],
+        )
+
 
 class GraphGenerators_TreeGraph(unittest.TestCase):
-    #note: our default is diff from networkx's default in this case (sink vs source)
+    # note: our default is diff from networkx's default in this case (sink vs source)
     def test_treeGraph_source_sink(self):
-        g = treeGraphGen(2,2)
-        self.assertEqual(list(g.edges()), [(1, 0), (2, 0), (3, 1), (4, 1), (5, 2), (6, 2)])
+        g = treeGraphGen(2, 2)
+        self.assertEqual(
+            list(g.edges()), [(1, 0), (2, 0), (3, 1), (4, 1), (5, 2), (6, 2)]
+        )
 
     def test_treeGraph_source(self):
-        g = treeGraphGen(2,2, kind='source')
-        self.assertEqual(list(g.edges()), [(0, 1), (0, 2), (1, 3), (1, 4), (2, 5), (2, 6)])
+        g = treeGraphGen(2, 2, kind="source")
+        self.assertEqual(
+            list(g.edges()), [(0, 1), (0, 2), (1, 3), (1, 4), (2, 5), (2, 6)]
+        )
 
     def test_treeGraph_bidir(self):
-        g = treeGraphGen(2,2, kind='bidir')
-        self.assertEqual(list(g.edges()), [
-            (1, 0), (1, 3), (1, 4), (0, 1), (0, 2), (2, 0),
-            (2, 5), (2, 6), (3, 1), (4, 1), (5, 2), (6, 2)
-        ])
+        g = treeGraphGen(2, 2, kind="bidir")
+        self.assertEqual(
+            list(g.edges()),
+            [
+                (1, 0),
+                (1, 3),
+                (1, 4),
+                (0, 1),
+                (0, 2),
+                (2, 0),
+                (2, 5),
+                (2, 6),
+                (3, 1),
+                (4, 1),
+                (5, 2),
+                (6, 2),
+            ],
+        )
 
-# this is the way I might test that the types assigned in Z's functions are correct 
-# I think it would be a good idea to abstract out that logic into a helper function 
-# so that the code isn't duplicated in every generator 
+
+# this is the way I might test that the types assigned in Z's functions are correct
+# I think it would be a good idea to abstract out that logic into a helper function
+# so that the code isn't duplicated in every generator
+
 
 class GraphGenerators_SetType(unittest.TestCase):
     def test_lineGraphGen_setType(self):
-        g = lineGraphGen(4, nodeTypeName='foo',edgeTypeName='bar')
+        g = lineGraphGen(4, nodeTypeName="foo", edgeTypeName="bar")
 
         # each node and edge should have the correct 'type' value
-        for k, v in nx.get_node_attributes(g, 'type').items():
-            self.assertEqual(v, 'foo')
+        for k, v in nx.get_node_attributes(g, "type").items():
+            self.assertEqual(v, "foo")
 
-        for k, v in nx.get_edge_attributes(g, 'type').items():
-            self.assertEqual(v, 'bar')
+        for k, v in nx.get_edge_attributes(g, "type").items():
+            self.assertEqual(v, "bar")
