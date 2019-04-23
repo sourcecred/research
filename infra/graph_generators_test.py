cd31f9ec9f4152bfd2b58bbc0fe76b279023f2fe
Michael Zargham <michael.zargham@gmail.com>
2019-04-22 19:40:33 -0700

    updated method names and added tests; tests fail

--- infra/graph_generators_test.py
+++ infra/graph_generators_test.py
@@ -5,58 +5,58 @@ This Module provides unit tests for functions for generating simple networks
 
 import networkx as nx
 import unittest
-from graph_generators import lineGraphGen, starGraphGen, treeGraphGen
+from graph_generators import line_graph_gen, star_graph_gen, tree_graph_gen, make_bidir, reverse_dir, set_types
 
 
-class GraphGenerators_LineGraph(unittest.TestCase):
-    def test_lineGraphGen_sanity(self):
-        g = lineGraphGen(4)
+class Graph_Generators_Line_Graph(unittest.TestCase):
+    def test_line_graph_gen_sanity(self):
+        g = line_graph_gen(4)
         self.assertEqual(list(g.edges()), [(0, 1), (1, 2), (2, 3)])
 
-    def test_lineGraphGen_bidir(self):
-        g = lineGraphGen(4, bidir=True)
+    def test_line_graph_gen_bidir(self):
+        g = line_graph_gen(4, bidir=True)
         # (node, neighbor)
         self.assertEqual(
             list(g.edges()), [(0, 1), (1, 0), (1, 2), (2, 1), (2, 3), (3, 2)]
         )
 
 
-class GraphGenerators_StarGraph(unittest.TestCase):
+class Graph_Generators_Star_Graph(unittest.TestCase):
 
     # how to validate that 'kind' is either 'source', 'sink', or 'bidir' ??
 
-    def test_starGraph_sink(self):
-        g = starGraphGen(4)
+    def test_star_graph_sink(self):
+        g = star_graph_gen(4)
         self.assertEqual(list(g.edges()), [(1, 0), (2, 0), (3, 0), (4, 0)])
 
-    def test_starGraph_source(self):
-        g = starGraphGen(4, kind="source")
+    def test_star_graph_source(self):
+        g = star_graph_gen(4, kind="source")
         self.assertEqual(list(g.edges()), [(0, 1), (0, 2), (0, 3), (0, 4)])
 
-    def test_starGraph_bidir(self):
-        g = starGraphGen(4, kind="bidir")
+    def test_star_graph_bidir(self):
+        g = star_graph_gen(4, kind="bidir")
         self.assertEqual(
             list(g.edges()),
             [(0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (2, 0), (3, 0), (4, 0)],
         )
 
 
-class GraphGenerators_TreeGraph(unittest.TestCase):
+class Graph_Generators_Tree_Graph(unittest.TestCase):
     # note: our default is diff from networkx's default in this case (sink vs source)
-    def test_treeGraph_source_sink(self):
-        g = treeGraphGen(2, 2)
+    def test_tree_graph_source_sink(self):
+        g = tree_graph_gen(2, 2)
         self.assertEqual(
             list(g.edges()), [(1, 0), (2, 0), (3, 1), (4, 1), (5, 2), (6, 2)]
         )
 
     def test_treeGraph_source(self):
-        g = treeGraphGen(2, 2, kind="source")
+        g = tree_graph_gen(2, 2, kind="source")
         self.assertEqual(
             list(g.edges()), [(0, 1), (0, 2), (1, 3), (1, 4), (2, 5), (2, 6)]
         )
 
-    def test_treeGraph_bidir(self):
-        g = treeGraphGen(2, 2, kind="bidir")
+    def test_tree_graph_bidir(self):
+        g = tree_graph_gen(2, 2, kind="bidir")
         self.assertEqual(
             list(g.edges()),
             [
@@ -76,14 +76,13 @@ class GraphGenerators_TreeGraph(unittest.TestCase):
         )
 
 
-# this is the way I might test that the types assigned in Z's functions are correct
-# I think it would be a good idea to abstract out that logic into a helper function
-# so that the code isn't duplicated in every generator
 
-
-class GraphGenerators_SetType(unittest.TestCase):
-    def test_lineGraphGen_setType(self):
-        g = lineGraphGen(4, nodeTypeName="foo", edgeTypeName="bar")
+class Graph_Generators_Set_Types(unittest.TestCase):
+    def test_set_types_str(self):
+        g =  nx.path_graph(3, create_using=nx.MultiDiGraph)
+        
+        g = set_types(g, node_types="foo", edge_types="bar")
+        
 
         # each node and edge should have the correct 'type' value
         for k, v in nx.get_node_attributes(g, "type").items():
@@ -91,3 +90,36 @@ class GraphGenerators_SetType(unittest.TestCase):
 
         for k, v in nx.get_edge_attributes(g, "type").items():
             self.assertEqual(v, "bar")
+    
+    def test_set_types_dict(self):
+        g =  nx.path_graph(3, create_using=nx.MultiDiGraph)
+        
+        node_dict = {0:"foo0", 1:"foo1", 2:"foo2"}
+        edge_dict = {(0,1,0):"bar0", (1,2,0):"bar1"}
+        
+        
+        g = set_types(g, node_types=node_dict, edge_types=edge_dict)
+        
+
+        # each node and edge should have the correct 'type' value
+        for k, v in nx.get_node_attributes(g, "type").items():
+            self.assertEqual(v, node_dict[k])
+
+        for k, v in nx.get_edge_attributes(g, "type").items():
+            self.assertEqual(v, edge_dict[k])
+
+class Graph_Generators_Reverse_Dir(unittest.TestCase):
+    def test_reverse_dir(self):
+        g =  nx.path_graph(3, create_using=nx.MultiDiGraph)
+        
+        g = reverse_dir(g)
+        
+        self.assertEqual(list(g.edges()), [(1, 0), (2, 1), (3, 2)])
+        
+class Graph_Generators_Make_Bidir(unittest.TestCase):
+    def test_make_bidir(self):
+        g =  nx.path_graph(3, create_using=nx.MultiDiGraph)
+        
+        g = make_bidir(g)
+        
+        self.assertEqual(list(g.edges()), [(0, 1), (1, 2), (2, 3),(1, 0), (2, 1), (3, 2)])
